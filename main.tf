989a0df0bd512954c192f8877aa9c2d7f1cdf8a2
Jiahe <yi.jiahe00@gmail.com>
2023-08-26 17:06:50 +0800

    Import Route53 zone and add query logging group

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-	  cloud {
+  cloud {
     organization = "yi-jiahe"
     workspaces {
       name = "aws_minecraft_server"
@@ -13,33 +13,15 @@ terraform {
     }
   }
 
-  required_version = ">= 1.2.0"
+  required_version = ">= 1.5.0"
+}
+
+# Required for Cloudwatch logging
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
 }
 
 provider "aws" {
-  region  = "ap-southeast-1"
-}
-
-resource "aws_instance" "minecraft_server" {
-  ami           = "ami-091a58610910a87a9"
-  instance_type = "t3.small"
-  vpc_security_group_ids = [aws_security_group.minecraft-sg.id]
-
-  user_data = "${file("init.sh")}"
-}
-
-resource "aws_security_group" "minecraft-sg" {
-  ingress {
-    from_port   = 25565
-    to_port     = 25565
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  region = var.region
 }
---
+++ resources.tf
@@ -0,0 +1,64 @@
+resource "aws_route53_zone" "minecraft_zone" {
+  name = var.domain
+}
+
+/*
+https://registry.terraform.io/providers/hashicorp/aws/5.12.0/docs/resources/route53_query_log
+There are restrictions on the configuration of query logging. 
+Notably, the CloudWatch log group must be in the us-east-1 region, a permissive CloudWatch log resource policy must be in place, and the Route53 hosted zone must be public. 
+*/
+
+resource "aws_cloudwatch_log_group" "aws_route53_minecraft_zone" {
+  provider = aws.us-east-1
+
+  name              = "/aws/route53/${aws_route53_zone.minecraft_zone.name}"
+  retention_in_days = 3
+}
+
+data "aws_iam_policy_document" "route53-query-logging-policy" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:log-group:/aws/route53/*"]
+
+    principals {
+      identifiers = ["route53.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
+resource "aws_cloudwatch_log_resource_policy" "route53-query-logging-policy" {
+  provider = aws.us-east-1
+
+  policy_document = data.aws_iam_policy_document.route53-query-logging-policy.json
+  policy_name     = "route53-query-logging-policy"
+}
+
+resource "aws_route53_query_log" "minecraft_zone" {
+  depends_on = [aws_cloudwatch_log_resource_policy.route53-query-logging-policy]
+
+  cloudwatch_log_group_arn = aws_cloudwatch_log_group.aws_route53_minecraft_zone.arn
+  zone_id                  = aws_route53_zone.minecraft_zone.zone_id
+}
+
+resource "aws_security_group" "minecraft-sg" {
+  description = "Allows inbound Minecraft traffic (TCP on port 25565) for all IPs"
+
+  ingress {
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
